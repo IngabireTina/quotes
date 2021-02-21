5e5faf7a5f8edf7c8b98c49e0e0063f209568ce6
IngabireTina <ingabire067@gmail.com>
2021-02-21 06:20:38 -0500

    added the downvote and upvote as well as delete in quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes: Quote[] = [];
+
+  toggleDetails(index) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+    
+  }
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote);
+
+  }
+
+  deleteQuote(isDelete, index) {
+    if(isDelete){
+      let toDelete = window.confirm(`ARE YOU SURE YOU WANT TO DELETE THIS QUOTE?`);
+      if(toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+  
+  increaseDownvote(isIncreaseDownvote, index) {
+    if(isIncreaseDownvote){
+      this.quote[index].numberOfDownvote += 1;
+    }
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
 }
