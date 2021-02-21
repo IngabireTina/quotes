886f7445b66cb61331066ad76744eeef2ddab277
IngabireTina <ingabire067@gmail.com>
2021-02-21 07:10:20 -0500

    caluculated the time passed

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -5,8 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePassedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): any {
+    let now:any = new Date();
+    var timeDifference = Math.abs(now - value);
+
+    var timeDifferenceSeconds = timeDifference * 0.001;
+   
+    const secondsInHour:any = 3600; //seconds in an hour
+    const secondsInMinute: any = 60; //seconds in a minute
+
+    var hoursPassed: any = (timeDifferenceSeconds / secondsInHour).toFixed();
+    var minutePassed: any = (timeDifferenceSeconds / secondsInMinute).toFixed();
+    var secondPassed: any = timeDifferenceSeconds.toFixed();
+
+    if(hoursPassed < 0) {
+      hoursPassed = 0;
+    }
+    if(minutePassed < 0) {
+      minutePassed = 0;
+    }
+
+
+    return `${hoursPassed}h ${minutePassed}min ${secondPassed}sec`;
   }
 
 }
