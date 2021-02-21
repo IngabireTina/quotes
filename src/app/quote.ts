34fd5f1494bfd819351fe0728dea1f8de27dce87
IngabireTina <ingabire067@gmail.com>
2021-02-21 06:37:33 -0500

    added the downvote and upvote as well as delete in quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
+    numberOfUpvote: number;
+    numberOfDownvote: number;
+    createdDate: Date;
+    showDetails: boolean;
+
+    constructor(public id:number, public fullname:string, public quoteTitle:string, public author:string){
+        this.numberOfUpvote = 0;
+        this.numberOfDownvote = 0;
+        this.createdDate = new Date();
+        this.showDetails = false;
+    }
 }
